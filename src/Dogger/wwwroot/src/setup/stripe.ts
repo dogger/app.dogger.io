5e54e2e1d3f31ccd9e3a402781db79e8027e7137
ffMathy <mathias.lorenzen@live.com>
2020-06-18 17:41:12 +0200

    updated build time process.

--- src/Dogger/wwwroot/src/setup/stripe.ts
+++ src/Dogger/wwwroot/src/setup/stripe.ts
@@ -1,5 +1,5 @@
 import {loadStripe} from '@stripe/stripe-js';
 
-const STRIPE_API_KEY = process.env.GATSBY_STRIPE_PUBLISHABLE_KEY || "pk_test_wlSYPpZrd6AXgjaQtQRt2NAu";
+const STRIPE_API_KEY = process.env.GATSBY_STRIPE_PUBLISHABLE_KEY || process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || "pk_test_wlSYPpZrd6AXgjaQtQRt2NAu";
 
 export const stripePromise = loadStripe(STRIPE_API_KEY);
