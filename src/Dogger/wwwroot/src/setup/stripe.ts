912cf53ef11927b2daefb8e5ce775af6db5d5a3c
ffMathy <mathias.lorenzen@live.com>
2020-06-19 14:00:37 +0200

    remove window check.

--- src/Dogger/wwwroot/src/setup/stripe.ts
+++ src/Dogger/wwwroot/src/setup/stripe.ts
@@ -1,10 +1,6 @@
 import {loadStripe} from '@stripe/stripe-js';
 
 const providedKey = process.env.GATSBY_STRIPE_PUBLISHABLE_KEY || process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
-if(!providedKey && typeof window !== "undefined") {
-    console.warn("No provided Stripe key was found! Will fall back to the test key.");
-}
-
 const STRIPE_API_KEY = providedKey || "pk_test_wlSYPpZrd6AXgjaQtQRt2NAu";
 
 export const stripePromise = loadStripe(STRIPE_API_KEY);
