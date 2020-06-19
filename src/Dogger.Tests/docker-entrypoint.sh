07562f10e5f726e51c1110f0fe997602b54556d5
ffMathy <mathias.lorenzen@live.com>
2020-06-19 16:21:59 +0200

    better code coverage reporting.

--- src/Dogger.Tests/docker-entrypoint.sh
+++ src/Dogger.Tests/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dotnet test --filter TestCategory=Unit --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
+dotnet test --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
 
 cp $(find ./TestResults -name "*.opencover.xml" | head -1) ./TestResults
