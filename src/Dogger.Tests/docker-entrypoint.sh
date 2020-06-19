66622f1617dca9ba80306c0a0b1f0e5541c83eed
ffMathy <mathias.lorenzen@live.com>
2020-06-19 15:39:04 +0200

    experimenting on entrypoints.

--- src/Dogger.Tests/docker-entrypoint.sh
+++ src/Dogger.Tests/docker-entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 dotnet test --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=json,cobertura,lcov,teamcity,opencover
+
+pwd
+echo "finding files"
+find ./src/Dogger.Tests/TestResults -name "*.opencover.xml"
