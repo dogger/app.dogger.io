5e876ef44e895328e77b465f72d241e711b67ebc
ffMathy <mathias.lorenzen@live.com>
2020-06-19 16:57:10 +0200

    added new collector.

---
+++ src/Dogger/Domain/Services/Provisioning/QueryPlanCollector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dogger.Domain.Services.Provisioning
+{
+    public class QueryPlanCollector
+    {
+    }
+
+    public class QueryPlan
+    {
+        public InstructionGroup[] Groups { get; }
+    }
+
+    public class InstructionGroup
+    {
+        public string Title { get; }
+        public Instruction[] Instructions { get; }
+    }
+
+    public class Instruction
+    {
+
+    }
+}
