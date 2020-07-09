a77a50c5f0b91842b883c552fff71190bca6257d
Mathias Lorenzen <mathias.lorenzen@live.com>
2020-07-09 21:08:23 +0200

    fix for update exception. fixes #397.

---
+++ src/Dogger/Infrastructure/Database/EntityFrameworkExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static bool IsUniqueConstraintViolation(this DbUpdateException exception)
+        {
+            return
+                exception.InnerException is SqlException sqlException &&
+                sqlException.Number == 2627;
+        }
+    }
+}
