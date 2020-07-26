67ab4016eba809019b68f4d8d14e900ece41186f
Mathias Lorenzen <mathias.lorenzen@live.com>
2020-07-26 14:43:35 +0200

    fix for logging.

--- src/Dogger/Controllers/Webhooks/Handlers/InstallationConfigurationPayloadHandler.cs
+++ src/Dogger/Controllers/Webhooks/Handlers/InstallationConfigurationPayloadHandler.cs
@@ -6,20 +6,28 @@ using Dogger.Controllers.Webhooks.Models;
 using Dogger.Domain.Commands.PullDog.AddPullDogToGitHubRepositories;
 using Dogger.Domain.Commands.PullDog.DeletePullDogRepository;
 using Dogger.Domain.Queries.PullDog.GetPullDogSettingsByGitHubInstallationId;
+using Dogger.Infrastructure.GitHub;
 using MediatR;
+using Serilog;
 
 namespace Dogger.Controllers.Webhooks.Handlers
 {
     public class InstallationConfigurationPayloadHandler : IConfigurationPayloadHandler
     {
         private readonly IMediator mediator;
+        private readonly IGitHubClientFactory gitHubClientFactory;
+        private readonly ILogger logger;
 
         public string Event => "installation_repositories";
 
         public InstallationConfigurationPayloadHandler(
-            IMediator mediator)
+            IMediator mediator,
+            IGitHubClientFactory gitHubClientFactory,
+            ILogger logger)
         {
             this.mediator = mediator;
+            this.gitHubClientFactory = gitHubClientFactory;
+            this.logger = logger;
         }
 
         public bool CanHandle(WebhookPayload payload)
@@ -35,7 +43,13 @@ namespace Dogger.Controllers.Webhooks.Handlers
                 new GetPullDogSettingsByGitHubInstallationIdQuery(
                     payload.Installation.Id));
             if (settings == null)
+            {
+                var client = await gitHubClientFactory.CreateInstallationClientAsync(payload.Installation.Id);
+                var currentUser = await client.User.Current();
+                this.logger.Error("An error occured with user {@User}.", currentUser);
+
                 throw new InvalidOperationException($"Could not find Pull Dog settings for an installation ID of {payload.Installation.Id}.");
+            }
 
             if (payload.RepositoriesRemoved != null)
             {
