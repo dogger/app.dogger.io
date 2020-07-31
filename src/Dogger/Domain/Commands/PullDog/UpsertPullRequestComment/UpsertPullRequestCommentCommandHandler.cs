3e4c6c3985f1afd06f9cac6d883648350bec8b2f
Mathias Lorenzen <mathias.lorenzen@live.com>
2020-07-31 16:04:10 +0200

    improved comment formatting.

--- src/Dogger/Domain/Commands/PullDog/UpsertPullRequestComment/UpsertPullRequestCommentCommandHandler.cs
+++ src/Dogger/Domain/Commands/PullDog/UpsertPullRequestComment/UpsertPullRequestCommentCommandHandler.cs
@@ -98,7 +98,7 @@ namespace Dogger.Domain.Commands.PullDog.UpsertPullRequestComment
                 configuration ?? new ConfigurationFile(), 
                 JsonFactory.GetOptions());
 
-            return GitHubCommentHelper.RenderSpoiler("Troubleshooting", $"Need help? Don't hesitate to <a href=\"https://github.com/dogger/app.dogger.io/issues/new\">file an issue</a> in our repository.{GitHubCommentHelper.RenderSpoiler("Configuration", $"**Initial**\n{GitHubCommentHelper.RenderCodeBlock("json", configurationJson)}\n\n**Trace ID**\n{request.TraceId ?? "No trace ID"}")}");
+            return GitHubCommentHelper.RenderSpoiler("Troubleshooting", $"Need help? Don't hesitate to <a href=\"https://github.com/dogger/app.dogger.io/issues/new\">file an issue</a> in our repository\n\n**Configuration**\n{GitHubCommentHelper.RenderCodeBlock("json", configurationJson)}\n\n**Trace ID**\n{request.TraceId ?? "No trace ID"}");
         }
 
         private static string RenderCommandsSection()
