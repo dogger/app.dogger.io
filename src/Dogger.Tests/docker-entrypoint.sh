c03a1f1a0e8a51629e2e826d161ddca5e463a1c9
Mathias Lorenzen <mathias.lorenzen@live.com>
2020-08-08 11:42:09 +0200

    fix for coverage.

--- src/Dogger.Tests/docker-entrypoint.sh
+++ src/Dogger.Tests/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dotnet test --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
 
-cp $(find ./TestResults/Dogger -name "*.opencover.xml" | head -1) ./TestResults
+cp $(find ./TestResults -name "*.opencover.xml" | head -1) ./TestResults/Dogger
