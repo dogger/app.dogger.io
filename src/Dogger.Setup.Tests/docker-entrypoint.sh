df3a42e6d2e3022ee445bc8e5a888e9de67aaf9c
ffMathy <mathias.lorenzen@live.com>
2020-08-24 18:38:26 +0200

    disabled coverage temporarily.

--- src/Dogger.Setup.Tests/docker-entrypoint.sh
+++ src/Dogger.Setup.Tests/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 dotnet test --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
 [ $? -eq 0 ] || exit 1
 
-cp $(find ./TestResults -name "*.opencover.xml" | head -1) ./TestResults/Dogger.Setup
+#cp $(find ./TestResults -name "*.opencover.xml" | head -1) ./TestResults/Dogger.Setup
