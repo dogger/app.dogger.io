a755bb7a8d5825a172144624146da8deabc95e8e
Mathias Lorenzen <mathias.lorenzen@live.com>
2020-08-26 21:12:47 +0200

    fix for docker entrypoints.

--- src/Dogger.Setup.Tests/docker-entrypoint.sh
+++ src/Dogger.Setup.Tests/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cd src/Dogger.Setup.Tests
 dotnet test --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
 [ $? -eq 0 ] || exit 1
 
-#cp $(find ./TestResults -name "*.opencover.xml" | head -1) ./TestResults/Dogger.Setup
+cp $(find ./TestResults -name "*.opencover.xml" | head -1) ./TestResults/Dogger.Setup
